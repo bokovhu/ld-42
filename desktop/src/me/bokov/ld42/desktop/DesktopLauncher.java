33a79d8b52800fed86bf004451ddea1547735a22
Botond Kovács <botond.kovacs@colonier.games>
2018-08-11 01:17:32 +0200

    Initial commit

---
+++ desktop/src/me/bokov/ld42/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.bokov.ld42.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.bokov.ld42.LD42Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD42Game(), config);
+	}
+}
